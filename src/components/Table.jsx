23487483de0553a5504615f5f165d41708b92c23
bestiannn <rebolledocofrebastian@gmail.com>
2023-01-15 03:33:29 -0300

    feat: game basic win with CPU

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -23,6 +23,7 @@ const Table = () => {
       { id: 9, char: null },
     ],
   ]);
+  const [lineWinner, setLineWinner] = useState([]);
 
   const handleClick = (id) => {
     if (turn !== char) return;
@@ -43,7 +44,128 @@ const Table = () => {
     toogleTurn();
   };
 
+  const checkWinner = () => {
+    if (
+      playsInfo[0][0].char === playsInfo[0][1].char &&
+      playsInfo[0][1].char === playsInfo[0][2].char &&
+      playsInfo[0][0].char !== null &&
+      playsInfo[0][1].char !== null &&
+      playsInfo[0][2].char !== null
+    ) {
+      setLineWinner([
+        playsInfo[0][0].id,
+        playsInfo[0][1].id,
+        playsInfo[0][2].id,
+      ]);
+      return playsInfo[0][0].char;
+    }
+    if (
+      playsInfo[1][0].char === playsInfo[1][1].char &&
+      playsInfo[1][1].char === playsInfo[1][2].char &&
+      playsInfo[1][0].char !== null &&
+      playsInfo[1][1].char !== null &&
+      playsInfo[1][2].char !== null
+    ) {
+      setLineWinner([
+        playsInfo[1][0].id,
+        playsInfo[1][1].id,
+        playsInfo[1][2].id,
+      ]);
+      return playsInfo[1][0].char;
+    }
+    if (
+      playsInfo[2][0].char === playsInfo[2][1].char &&
+      playsInfo[2][1].char === playsInfo[2][2].char &&
+      playsInfo[2][0].char !== null &&
+      playsInfo[2][1].char !== null &&
+      playsInfo[2][2].char !== null
+    ) {
+      setLineWinner([
+        playsInfo[2][0].id,
+        playsInfo[2][1].id,
+        playsInfo[2][2].id,
+      ]);
+      return playsInfo[2][0].char;
+    }
+    if (
+      playsInfo[0][0].char === playsInfo[1][0].char &&
+      playsInfo[1][0].char === playsInfo[2][0].char &&
+      playsInfo[0][0].char !== null &&
+      playsInfo[1][0].char !== null &&
+      playsInfo[2][0].char !== null
+    ) {
+      setLineWinner([
+        playsInfo[0][0].id,
+        playsInfo[1][0].id,
+        playsInfo[2][0].id,
+      ]);
+      return playsInfo[0][0].char;
+    }
+    if (
+      playsInfo[0][1].char === playsInfo[1][1].char &&
+      playsInfo[1][1].char === playsInfo[2][1].char &&
+      playsInfo[0][1].char !== null &&
+      playsInfo[1][1].char !== null &&
+      playsInfo[2][1].char !== null
+    ) {
+      setLineWinner([
+        playsInfo[0][1].id,
+        playsInfo[1][1].id,
+        playsInfo[2][1].id,
+      ]);
+      return playsInfo[0][1].char;
+    }
+    if (
+      playsInfo[0][2].char === playsInfo[1][2].char &&
+      playsInfo[1][2].char === playsInfo[2][2].char &&
+      playsInfo[0][2].char !== null &&
+      playsInfo[1][2].char !== null &&
+      playsInfo[2][2].char !== null
+    ) {
+      setLineWinner([
+        playsInfo[0][2].id,
+        playsInfo[1][2].id,
+        playsInfo[2][2].id,
+      ]);
+      return playsInfo[0][2].char;
+    }
+    if (
+      playsInfo[0][0].char === playsInfo[1][1].char &&
+      playsInfo[1][1].char === playsInfo[2][2].char &&
+      playsInfo[0][0].char !== null &&
+      playsInfo[1][1].char !== null &&
+      playsInfo[2][2].char !== null
+    ) {
+      setLineWinner([
+        playsInfo[0][0].id,
+        playsInfo[1][1].id,
+        playsInfo[2][2].id,
+      ]);
+      return playsInfo[0][0].char;
+    }
+    if (
+      playsInfo[0][2].char === playsInfo[1][1].char &&
+      playsInfo[1][1].char === playsInfo[2][0].char &&
+      playsInfo[0][2].char !== null &&
+      playsInfo[1][1].char !== null &&
+      playsInfo[2][0].char !== null
+    ) {
+      setLineWinner([
+        playsInfo[0][2].id,
+        playsInfo[1][1].id,
+        playsInfo[2][0].id,
+      ]);
+      return playsInfo[0][2].char;
+    }
+
+    return null;
+  };
+
   useEffect(() => {
+    if (checkWinner()) return;
+
+    console.log(lineWinner);
+
     // check if there is a char in null
     const hasNull = playsInfo.flat().some((play) => !play.char);
     if (!hasNull) return;
@@ -75,7 +197,9 @@ const Table = () => {
       {playsInfo.map((row) =>
         row.map(({ id, char }) => (
           <div
-            className="grid place-content-center rounded-xl bg-surface2"
+            className={`grid place-content-center rounded-xl bg-surface2 ${
+              lineWinner.includes(id) && "bg-green text-base"
+            }`}
             key={id}
             onClick={() => handleClick(id)}
           >
