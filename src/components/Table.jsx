f1bff6654911213efee2fce6a016ec1fe3109d0e
bestiannn <rebolledocofrebastian@gmail.com>
2023-01-17 04:03:48 -0300

    refactor: logic game table refactorized

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -7,235 +7,135 @@ const Table = () => {
   const { turn, toogleTurn, addWin, addLoss, addTie } = useGameStore();
 
   const [playsInfo, setPlaysInfo] = useState([
-    [
-      { id: 1, char: null },
-      { id: 2, char: null },
-      { id: 3, char: null },
-    ],
-    [
-      { id: 4, char: null },
-      { id: 5, char: null },
-      { id: 6, char: null },
-    ],
-    [
-      { id: 7, char: null },
-      { id: 8, char: null },
-      { id: 9, char: null },
-    ],
+    { id: 1, char: null },
+    { id: 2, char: null },
+    { id: 3, char: null },
+    { id: 4, char: null },
+    { id: 5, char: null },
+    { id: 6, char: null },
+    { id: 7, char: null },
+    { id: 8, char: null },
+    { id: 9, char: null },
   ]);
   const [lineWinner, setLineWinner] = useState([]);
 
   const handleClick = (id) => {
     if (turn !== char) return;
-    if (playsInfo.flat().find((play) => play.id === id).char) return;
+    if (playsInfo.find((play) => play.id === id).char) return;
 
-    const newPlaysInfo = playsInfo.map((row) =>
-      row.map((play) => {
-        if (play.id === id) {
-          return {
-            ...play,
-            char,
-          };
-        }
-        return play;
-      })
-    );
+    const newPlaysInfo = playsInfo.map((play) => {
+      if (play.id === id) {
+        return {
+          ...play,
+          char,
+        };
+      }
+      return play;
+    });
     setPlaysInfo(newPlaysInfo);
     toogleTurn();
   };
 
   const checkWinner = () => {
+    for (let i = 0; i < 9; i += 3) {
+      if (
+        playsInfo[i].char &&
+        playsInfo[i].char === playsInfo[i + 1].char &&
+        playsInfo[i].char === playsInfo[i + 2].char
+      ) {
+        setLineWinner([i, i + 1, i + 2]);
+        return playsInfo[i].char;
+      }
+    }
+
+    for (let i = 0; i < 3; i++) {
+      if (
+        playsInfo[i].char &&
+        playsInfo[i].char === playsInfo[i + 3].char &&
+        playsInfo[i].char === playsInfo[i + 6].char
+      ) {
+        setLineWinner([i, i + 3, i + 6]);
+        return playsInfo[i].char;
+      }
+    }
+
     if (
-      playsInfo[0][0].char === playsInfo[0][1].char &&
-      playsInfo[0][1].char === playsInfo[0][2].char &&
-      playsInfo[0][0].char !== null &&
-      playsInfo[0][1].char !== null &&
-      playsInfo[0][2].char !== null
+      playsInfo[0].char &&
+      playsInfo[0].char === playsInfo[4].char &&
+      playsInfo[0].char === playsInfo[8].char
     ) {
-      setLineWinner([
-        playsInfo[0][0].id,
-        playsInfo[0][1].id,
-        playsInfo[0][2].id,
-      ]);
-      return playsInfo[0][0].char;
+      setLineWinner([0, 4, 8]);
+      return playsInfo[0].char;
     }
     if (
-      playsInfo[1][0].char === playsInfo[1][1].char &&
-      playsInfo[1][1].char === playsInfo[1][2].char &&
-      playsInfo[1][0].char !== null &&
-      playsInfo[1][1].char !== null &&
-      playsInfo[1][2].char !== null
+      playsInfo[2].char &&
+      playsInfo[2].char === playsInfo[4].char &&
+      playsInfo[2].char === playsInfo[6].char
     ) {
-      setLineWinner([
-        playsInfo[1][0].id,
-        playsInfo[1][1].id,
-        playsInfo[1][2].id,
-      ]);
-      return playsInfo[1][0].char;
-    }
-    if (
-      playsInfo[2][0].char === playsInfo[2][1].char &&
-      playsInfo[2][1].char === playsInfo[2][2].char &&
-      playsInfo[2][0].char !== null &&
-      playsInfo[2][1].char !== null &&
-      playsInfo[2][2].char !== null
-    ) {
-      setLineWinner([
-        playsInfo[2][0].id,
-        playsInfo[2][1].id,
-        playsInfo[2][2].id,
-      ]);
-      return playsInfo[2][0].char;
-    }
-    if (
-      playsInfo[0][0].char === playsInfo[1][0].char &&
-      playsInfo[1][0].char === playsInfo[2][0].char &&
-      playsInfo[0][0].char !== null &&
-      playsInfo[1][0].char !== null &&
-      playsInfo[2][0].char !== null
-    ) {
-      setLineWinner([
-        playsInfo[0][0].id,
-        playsInfo[1][0].id,
-        playsInfo[2][0].id,
-      ]);
-      return playsInfo[0][0].char;
-    }
-    if (
-      playsInfo[0][1].char === playsInfo[1][1].char &&
-      playsInfo[1][1].char === playsInfo[2][1].char &&
-      playsInfo[0][1].char !== null &&
-      playsInfo[1][1].char !== null &&
-      playsInfo[2][1].char !== null
-    ) {
-      setLineWinner([
-        playsInfo[0][1].id,
-        playsInfo[1][1].id,
-        playsInfo[2][1].id,
-      ]);
-      return playsInfo[0][1].char;
-    }
-    if (
-      playsInfo[0][2].char === playsInfo[1][2].char &&
-      playsInfo[1][2].char === playsInfo[2][2].char &&
-      playsInfo[0][2].char !== null &&
-      playsInfo[1][2].char !== null &&
-      playsInfo[2][2].char !== null
-    ) {
-      setLineWinner([
-        playsInfo[0][2].id,
-        playsInfo[1][2].id,
-        playsInfo[2][2].id,
-      ]);
-      return playsInfo[0][2].char;
-    }
-    if (
-      playsInfo[0][0].char === playsInfo[1][1].char &&
-      playsInfo[1][1].char === playsInfo[2][2].char &&
-      playsInfo[0][0].char !== null &&
-      playsInfo[1][1].char !== null &&
-      playsInfo[2][2].char !== null
-    ) {
-      setLineWinner([
-        playsInfo[0][0].id,
-        playsInfo[1][1].id,
-        playsInfo[2][2].id,
-      ]);
-      return playsInfo[0][0].char;
-    }
-    if (
-      playsInfo[0][2].char === playsInfo[1][1].char &&
-      playsInfo[1][1].char === playsInfo[2][0].char &&
-      playsInfo[0][2].char !== null &&
-      playsInfo[1][1].char !== null &&
-      playsInfo[2][0].char !== null
-    ) {
-      setLineWinner([
-        playsInfo[0][2].id,
-        playsInfo[1][1].id,
-        playsInfo[2][0].id,
-      ]);
-      return playsInfo[0][2].char;
+      setLineWinner([2, 4, 6]);
+      return playsInfo[2].char;
     }
 
     return null;
   };
 
-  useEffect(() => {
-    if (checkWinner()) {
-      if (checkWinner() === char) {
-        addWin();
-      } else {
-        addLoss();
-      }
+  const resetPlaysInfo = () => {
+    setPlaysInfo([
+      { id: 1, char: null },
+      { id: 2, char: null },
+      { id: 3, char: null },
+      { id: 4, char: null },
+      { id: 5, char: null },
+      { id: 6, char: null },
+      { id: 7, char: null },
+      { id: 8, char: null },
+      { id: 9, char: null },
+    ]);
+  };
 
-      setPlaysInfo([
-        [
-          { id: 1, char: null },
-          { id: 2, char: null },
-          { id: 3, char: null },
-        ],
-        [
-          { id: 4, char: null },
-          { id: 5, char: null },
-          { id: 6, char: null },
-        ],
-        [
-          { id: 7, char: null },
-          { id: 8, char: null },
-          { id: 9, char: null },
-        ],
-      ]);
+  useEffect(() => {
+    // Win or Lose
+    if (checkWinner()) {
+      if (checkWinner() === char) addWin();
+      else addLoss();
+
+      resetPlaysInfo();
       setLineWinner([]);
       return;
     }
-    if (playsInfo.flat().every((play) => play.char !== null)) {
+
+    // Tie
+    if (playsInfo.every((play) => play.char !== null)) {
       addTie();
-      setPlaysInfo([
-        [
-          { id: 1, char: null },
-          { id: 2, char: null },
-          { id: 3, char: null },
-        ],
-        [
-          { id: 4, char: null },
-          { id: 5, char: null },
-          { id: 6, char: null },
-        ],
-        [
-          { id: 7, char: null },
-          { id: 8, char: null },
-          { id: 9, char: null },
-        ],
-      ]);
+      resetPlaysInfo();
       setLineWinner([]);
       return;
     }
 
     // check if there is a char in null
-    const hasNull = playsInfo.flat().some((play) => !play.char);
+    const hasNull = playsInfo.some((play) => !play.char);
     let enemyPlay;
     if (!hasNull) return;
+    // Enemy play
     else if (char !== turn) {
-      const emptyPlay = playsInfo.flat().find((play) => !play.char).id;
+      const emptyPlay = playsInfo.find((play) => !play.char).id;
 
       enemyPlay = setTimeout(() => {
         setPlaysInfo((prevPlaysInfo) => {
-          const newPlaysInfo = prevPlaysInfo.map((row) =>
-            row.map((play) => {
-              if (play.id === emptyPlay) {
-                return {
-                  ...play,
-                  char: char === "X" ? "O" : "X",
-                };
-              }
-              return play;
-            })
-          );
+          const newPlaysInfo = prevPlaysInfo.map((play) => {
+            if (play.id === emptyPlay) {
+              return {
+                ...play,
+                char: char === "X" ? "O" : "X",
+              };
+            }
+            return play;
+          });
           return newPlaysInfo;
         });
+
         toogleTurn();
-      }, 2000);
+      }, 1000);
     }
 
     return () => clearTimeout(enemyPlay);
@@ -243,19 +143,17 @@ const Table = () => {
 
   return (
     <div className="grid flex-1 grid-cols-3 gap-5 text-center">
-      {playsInfo.map((row) =>
-        row.map(({ id, char }) => (
-          <div
-            className={`grid place-content-center rounded-xl bg-surface2 ${
-              lineWinner.includes(id) && "bg-green text-base"
-            }`}
-            key={id}
-            onClick={() => handleClick(id)}
-          >
-            {char}
-          </div>
-        ))
-      )}
+      {playsInfo.map(({ id, char }) => (
+        <div
+          className={`grid place-content-center rounded-xl bg-surface2 ${
+            lineWinner.includes(id) && "bg-green text-base"
+          }`}
+          key={id}
+          onClick={() => handleClick(id)}
+        >
+          {char}
+        </div>
+      ))}
     </div>
   );
 };
